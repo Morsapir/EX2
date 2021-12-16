f278a29d33fb2e8edab044b723457b530495ad01
Morsapir <92923336+Morsapir@users.noreply.github.com>
2021-12-16 12:01:56 +0200

    Add files via upload

---
+++ src/Ex2.java
@@ -0,0 +1,42 @@
+import api.DirectedWeightedGraph;
+import api.DirectedWeightedGraphAlgorithms;
+
+/**
+ * This class is the main class for Ex2 - your implementation will be tested using this class.
+ */
+public class Ex2 {
+    /**
+     * This static function will be used to test your implementation
+     * @param json_file - a json file (e.g., G1.json - G3.gson)
+     * @return
+     */
+    public static DirectedWeightedGraph getGrapg(String json_file) {
+        DirectedWeightedGraph ans = new directedWightedGraph();
+        directedWeightedGraphAlgorithms graph = new directedWeightedGraphAlgorithms();
+        graph.init(ans);
+        graph.load(json_file);
+        return graph.getGraph();
+    }
+    /**
+     * This static function will be used to test your implementation
+     * @param json_file - a json file (e.g., G1.json - G3.gson)
+     * @return
+     */
+    public static DirectedWeightedGraphAlgorithms getGrapgAlgo(String json_file) {
+        DirectedWeightedGraph ans = new directedWightedGraph();
+        directedWeightedGraphAlgorithms graph = new directedWeightedGraphAlgorithms();
+        graph.init(ans);
+        graph.load(json_file);
+        return graph;
+    }
+    /**
+     * This static function will run your GUI using the json fime.
+     * @param json_file - a json file (e.g., G1.json - G3.gson)
+     *
+     */
+    public static void runGUI(String json_file) {
+        DirectedWeightedGraphAlgorithms alg = getGrapgAlgo(json_file);
+
+
+    }
+}
---
+++ src/directedWightedGraph.java
@@ -0,0 +1,157 @@
+import api.DirectedWeightedGraph;
+import api.EdgeData;
+import api.NodeData;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+
+public class directedWightedGraph implements DirectedWeightedGraph {
+    private HashMap<Integer, NodeData> nodeData;
+    private HashMap<Integer, HashMap<Integer, EdgeData>> edges;
+    private HashMap<Integer, HashMap<Integer, EdgeData>> edges1;
+    private int edgeSize;
+    private int mc;
+
+    public directedWightedGraph(directedWightedGraph graph){
+        this.nodeData = graph.nodeData;
+        this.edges = graph.edges;
+        this.edges1 = graph.edges1;
+        this.edgeSize=graph.edgeSize;
+        this.mc=graph.mc;
+    }
+
+    public directedWightedGraph(){
+        this.nodeData = new HashMap<>();
+        this.edges = new HashMap<>();
+        this.edges1 = new HashMap<>();
+        this.edgeSize = 0;
+        this.mc = 0;
+    }
+
+    @Override
+    public NodeData getNode(int key) {
+        return this.nodeData.get(key);
+    }
+
+    @Override
+    public EdgeData getEdge(int src, int dest) {
+        return this.edges.get(src).get(dest);
+    }
+
+    @Override
+    public void addNode(NodeData n) {
+        this.nodeData.put(n.getKey(),n);
+        this.mc++;
+    }
+
+    @Override
+    public void connect(int src, int dest, double w) {
+        if(nodeData.containsKey(src)||!nodeData.containsKey(dest)||src == dest) {
+            throw new RuntimeException();
+        }
+        EdgeData e = new edgeData(src,dest,w," ",0);
+        if(edges.containsKey(src))
+            edges.get(src).put(dest,e);
+        else
+        {
+            HashMap<Integer,EdgeData> p = new HashMap<Integer,EdgeData>();
+            p.put(dest,e);
+            edges.put(src,p);
+        }
+        if(edges1.containsKey(dest))
+            edges1.get(dest).put(src,e);
+        else
+        {
+            HashMap<Integer,EdgeData> p = new HashMap<Integer,EdgeData>();
+            p.put(src,e);
+            edges1.put(dest,p);
+        }
+        this.edgeSize++;
+        this.mc++;
+    }
+
+
+    @Override
+    public Iterator<NodeData> nodeIter() {
+        int counter = mc;
+        if(mc != counter)
+            throw new RuntimeException(" the graph was changed since the iterator was constructed");
+        return nodeData.values().iterator();
+            }
+
+    @Override
+    public Iterator<EdgeData> edgeIter() {
+            List<EdgeData> list = new ArrayList<>();
+            Iterator<NodeData> i=nodeIter();
+            while (i.hasNext()){
+                Iterator<EdgeData> iE=edgeIter(i.next().getKey());
+                while (iE.hasNext()){
+                    list.add(iE.next());
+                }
+            }
+            return list.iterator();
+        }
+
+
+
+    @Override
+    public Iterator<EdgeData> edgeIter(int node_id) {
+        return this.edges.get(node_id).values().iterator();     }
+
+    @Override
+    public NodeData removeNode(int key) {
+        if(!this.nodeData.containsKey(key))
+            return null;
+        NodeData vertex = nodeData.remove(key);
+        HashMap<Integer, EdgeData> e = edges.get(key);
+        HashMap<Integer, EdgeData> e1 = edges1.get(key);
+        Iterator<EdgeData> i = e.values().iterator();
+        Iterator<EdgeData> k = e1.values().iterator();
+
+        while (i.hasNext()){
+            EdgeData runner = i.next();
+            EdgeData eData = edges.get(runner.getSrc()).get(runner.getDest());
+            edges1.get(eData.getDest()).remove(eData.getSrc());
+            edgeSize --;
+        }
+
+        while (k.hasNext()){
+            EdgeData runner = k.next();
+            EdgeData eData = edges1.get(runner.getDest()).get(runner.getSrc());
+            edges.get(eData.getSrc()).remove(eData.getDest());
+            edgeSize --;
+
+        }
+        this.edges.remove(key);
+        this.mc ++;
+        return vertex;
+    }
+
+    @Override
+    public EdgeData removeEdge(int src, int dest) {
+            if (this.edges.get(src).containsKey(dest)){
+                this.edges.get(src).remove(dest);
+                this.edges1.get(dest).remove(src);
+                this.edgeSize--;
+                this.mc++;
+            }
+            return null;
+        }
+
+    @Override
+    public int nodeSize() {
+        return this.nodeData.size();
+    }
+
+    @Override
+    public int edgeSize() {
+        return this.edgeSize;
+    }
+
+    @Override
+    public int getMC() {
+        return this.mc;
+    }
+}
